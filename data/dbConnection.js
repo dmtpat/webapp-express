5bdcf038f061aea064f2883b6423c44bcd20c249
Pat <dmtpatricica@email.com>
2026-03-18 20:25:36 +0100

    db connection

---
+++ data/dbConnection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const dbConfiguration = {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "movies_db"
+}
+
+function onDatabaseConnection(error) {
+    if (error) throw error;
+    console.log("Connessione a MySQL avvenuta");
+}
+
+const dbConnection = mysql.createConnection(dbConfiguration);
+dbConnection.connect(onDatabaseConnection);
+
+module.exports = dbConnection;
