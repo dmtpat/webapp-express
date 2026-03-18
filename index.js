88d38fa6eebcec50dd9d586ec15931b564f0d819
Pat <dmtpatricica@email.com>
2026-03-18 19:27:32 +0100

    base server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Benvenuto!")
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log("Cliamata di base del server");
+    res.send("Server della webApp!");
+})
+
+app.listen(port, () => {
+    console.log("La chiamata è stata accettata");
+})
